531e4d83e06e856c4020d43fd66c6da655c4003f
leesh235 <wksehs23@naver.com>
2021-04-09 22:29:21 +0900

    Create a news list

--- requsets/get_news.py
+++ requsets/get_news.py
@@ -10,7 +10,11 @@ soup = BeautifulSoup(res.text,"lxml")
 #가장 최신 페이지의 뉴스리스트을 가져오기
 news_lists = soup.find_all("div",{"class":"news_list_"})
 
+news = []
+
 for news_list in news_lists:
     news_name = news_list.find("h4")["title"]
     news_link = "https://www.itworld.co.kr/" + news_list.find("a")["href"]
-    print(news_name, news_link)
+    latest_news = {"title":news_name, "link":news_link}
+    news.append(latest_news)
+print(news[0].get("title"))

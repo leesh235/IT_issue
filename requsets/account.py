3c02f2e858e77a872338babb1ac562667cbfb612
leesh235 <wksehs23@naver.com>
2021-04-09 23:52:12 +0900

    add email system

--- requsets/account.py
+++ requsets/account.py
@@ -1,2 +1,2 @@
-EMAIL_ADDRESS = "메일 주소" # 주소
+EMAIL_ADDRESS = "이메일주소" # 주소
 EMAIL_PASSWORD = "앱 비밀번호" # 앱 비밀번호
--- requsets/get_news.py
+++ requsets/get_news.py
@@ -1,6 +1,15 @@
 import requests
+import time
 import csv
 from bs4 import BeautifulSoup
+import smtplib
+from account import *
+from email.message import EmailMessage
+
+msg = EmailMessage()
+msg["Subject"] = "Latest IT-issue"
+msg["From"] = EMAIL_ADDRESS
+msg["To"] = EMAIL_ADDRESS
 
 url = "https://www.itworld.co.kr/news"
 res = requests.get(url)
@@ -16,10 +25,18 @@ soup = BeautifulSoup(res.text,"lxml")
 news_lists = soup.find_all("div",{"class":"news_list_"})
 
 #각각의 뉴스를 객체로 만들어 list에 저장
-
+article=""
 for news_list in news_lists:
     news_name = news_list.find("h4")["title"]
     news_link = "https://www.itworld.co.kr/" + news_list.find("a")["href"]
-    news= [news_name, news_link]
-    print(news)
+    news = [news_name, news_link]
     writer.writerow(news)
+    article = article + "\n" + news_name+" "+news_link
+    msg.set_content(article)
+
+#이메일보내기
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)

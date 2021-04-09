eb5688a925ecf18eaa15faa7a349b5b2c2a60e42
leesh235 <wksehs23@naver.com>
2021-04-09 22:50:29 +0900

    Create csv file

--- requsets/get_news.py
+++ requsets/get_news.py
@@ -1,20 +1,25 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 url = "https://www.itworld.co.kr/news"
 res = requests.get(url)
 res.raise_for_status()
 
+filename = "IT-issue.csv"
+f=open(filename, "w", encoding="utf8", newline="")
+writer = csv.writer(f)
+
 soup = BeautifulSoup(res.text,"lxml")
 
 #가장 최신 페이지의 뉴스리스트을 가져오기
 news_lists = soup.find_all("div",{"class":"news_list_"})
 
-news = []
+#각각의 뉴스를 객체로 만들어 list에 저장
 
 for news_list in news_lists:
     news_name = news_list.find("h4")["title"]
     news_link = "https://www.itworld.co.kr/" + news_list.find("a")["href"]
-    latest_news = {"title":news_name, "link":news_link}
-    news.append(latest_news)
-print(news[0].get("title"))
+    news= [news_name, news_link]
+    print(news)
+    writer.writerow(news)

6c31a98adb9dc0189ba9045268e01aae8c4ba5db
leesh235 <wksehs23@naver.com>
2021-04-09 21:47:30 +0900

    Scraping the latest News

---
+++ requsets/3_requsets.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.itworld.co.kr/news"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text,"lxml")
+
+#가장 최신 페이지의 뉴스리스트을 가져오기
+news_lists = soup.find_all("div",{"class":"news_list_"})
+
+for news_list in news_lists:
+    news_name = news_list.find("h4")["title"]
+    news_link = "https://www.itworld.co.kr/" + news_list.find("a")["href"]
+    print(news_name, news_link)
